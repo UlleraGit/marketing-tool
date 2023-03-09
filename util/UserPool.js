faf08414d495d17aee5d2e955da832f7354dc057
Zsombor hidasi <zsombor.hidasi@ullera.com>
2023-03-09 02:59:19 +0100

    push 09.03.2023 v1.0

--- util/UserPool.js
+++ util/UserPool.js
@@ -3,7 +3,12 @@ import { CognitoUserPool,CookieStorage } from "amazon-cognito-identity-js";
 const poolData = {
     UserPoolId:'eu-central-1_HYkj5z08r',
     ClientId: '6gapt5uqq79fome27kngjuetht',
-    Storage: new CookieStorage({domain: process.env.COOKIE_DOMAIN, secure:false})
+    cookieStorage: {
+        domain: 'localhost',
+        secure: false,
+        path: '/',
+        expires: 365,
+      },
 }
 
 export default new CognitoUserPool(poolData);
