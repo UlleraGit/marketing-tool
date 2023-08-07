73b63f7e6aac22c6b8c8c34010cbbc7365b0cc98
Zsombor hidasi <zsombor.hidasi@ullera.com>
2023-08-07 16:31:07 +0200

    v1.05 official icon update

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,13 @@ import lightTheme from "../styles/theme/lightTheme";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import Script from "next/script";
+import Head from "next/head";
 
 const MyApp = ({ Component, pageProps }) => {
-  return (
+  return (<>
+    <Head>
+    <link rel="shortcut icon" href="/public/Icon-dc-trans.ico" />
+    </Head>
     <div>
       <ThemeProvider theme={lightTheme}>
         <CssBaseline />
@@ -13,6 +17,7 @@ const MyApp = ({ Component, pageProps }) => {
       </ThemeProvider>
       <Script async src="https://js.stripe.com/v3/pricing-table.js" />
     </div>
+  </>
   );
 };
 export default MyApp;
