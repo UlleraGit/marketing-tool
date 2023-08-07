38bc1f4bad63ec7ff0811d4f570108c87cd015cd
Zsombor hidasi <zsombor.hidasi@ullera.com>
2023-08-07 06:57:18 +0200

    v1.01

---
+++ .eslintric.js
@@ -0,0 +1,14 @@
+module.exports = {
+    root: true,
+    env: {
+      browser: true,
+      node: true,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:@next/next/recommended',
+    ],
+    rules: {},
+  }
+  
